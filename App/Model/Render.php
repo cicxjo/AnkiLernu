99ebd49d43a98d500b194cc9a1903a0ca7e29df2
Cyril Augier <cicxjo@posteo.net>
2023-10-25 09:39:22 +0200

    Ajoute la classe qui permet d‘afficher les vues

---
+++ App/Model/Render.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Model;
+
+class Render
+{
+    private string $path;
+    private string $layout;
+    private string $template;
+
+    public function __construct(string $layout)
+    {
+        $this->path = realpath(getcwd() . '/View/');
+        $this->layout = realpath($this->path . '/Layout/' . $layout . '.phtml');
+    }
+
+    public function setTemplate(string $template): self
+    {
+        $this->template = realpath($this->path . '/Template/' . $template . '.phtml');
+
+        return $this;
+    }
+
+    public function process(array $vars = []): void
+    {
+        foreach ($vars as $key => $value) {
+            $$key = $value;
+        }
+
+        ob_start();
+        require_once($this->template);
+        $vars = ob_get_clean();
+        require_once($this->layout);
+    }
+}
