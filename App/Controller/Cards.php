8a9455aeb74e539bf872920df0d1667ea7171abb
Cyril Augier <cicxjo@posteo.net>
2023-11-28 10:42:33 +0100

    Vérifie correctement les paramètres POST pour générer les cartes

--- App/Controller/Cards.php
+++ App/Controller/Cards.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Data\Languages;
 use App\Model\Entity\Card as CardEntity;
+use App\Model\Exception\HTTPException;
 use App\Model\Exception\ScraperException;
 use App\Model\Manager\Card as CardManager;
 use App\Model\Render;
@@ -17,16 +18,10 @@ class Cards
 {
     private Render $render;
     private int $cacheTime = 7 * 24 * 60 * 60;
-    private bool $languageIsValid;
 
     public function __construct()
     {
         $this->render = new Render('Raw');
-    }
-
-    private function languageExists()
-    {
-        return isset($_POST['language']) && key_exists($_POST['language'], Languages::$all);
     }
 
     private function getCards(array $words, $language): array
@@ -38,44 +33,38 @@ class Cards
 
         $deck = [];
 
-        if ($this->languageExists()) {
-            $this->languageIsValid = true;
+        foreach ($words as $word) {
+            if (empty($word) || ctype_space($word)) {
+                continue;
+            } else {
+                $word = trim($word);
+            }
 
-            foreach ($words as $word) {
-                if (empty($word) || ctype_space($word)) {
-                    continue;
-                } else {
-                    $word = trim($word);
-                }
+            $cardEntity = $cardManager->get($language, $word);
 
-                $cardEntity = $cardManager->get($language, $word);
+            try {
+                if ($cardEntity) {
+                    $currentDate = (new DateTime())->setTimezone(new DateTimeZone('UTC'))
+                                                   ->format('Y-m-d H:i:s');
+                    $cardSyncDate = $cardEntity->getSyncAt();
 
-                try {
-                    if ($cardEntity) {
-                        $currentDate = (new DateTime())->setTimezone(new DateTimeZone('UTC'))
-                                                       ->format('Y-m-d H:i:s');
-                        $cardSyncDate = $cardEntity->getSyncAt();
-
-                        if (strtotime($currentDate) > strtotime($cardSyncDate) + $this->cacheTime) {
-                            $translation = $scraper->execute($word, $language, $token);
-                            $cardEntity = (new CardEntity())->setWord($word)
-                                                        ->setTranslation($translation)
-                                                        ->setSyncAt($currentDate);
-                            $cardManager->update($cardEntity, $language);
-                        }
-                    } else {
+                    if (strtotime($currentDate) > strtotime($cardSyncDate) + $this->cacheTime) {
                         $translation = $scraper->execute($word, $language, $token);
                         $cardEntity = (new CardEntity())->setWord($word)
-                                                    ->setTranslation($translation);
-                        $cardManager->insert($language, $cardEntity);
+                                                    ->setTranslation($translation)
+                                                    ->setSyncAt($currentDate);
+                        $cardManager->update($cardEntity, $language);
                     }
-                        $deck[] = $cardEntity;
-                } catch (ScraperException $exception) {
-                    $deck[] = $exception;
+                } else {
+                    $translation = $scraper->execute($word, $language, $token);
+                    $cardEntity = (new CardEntity())->setWord($word)
+                                                ->setTranslation($translation);
+                    $cardManager->insert($language, $cardEntity);
                 }
+                    $deck[] = $cardEntity;
+            } catch (ScraperException $exception) {
+                $deck[] = $exception;
             }
-        } else {
-            $this->languageIsValid = false;
         }
 
         return $deck;
@@ -83,20 +72,26 @@ class Cards
 
     public function generate(): void
     {
-        $language = $_POST['language'];
-        $words = array_values(array_filter(
-            $_POST,
-            fn($key) => preg_match('/^word-/', $key),
-            ARRAY_FILTER_USE_KEY
-        ));
+        if (isset($_POST['language']) && array_key_exists($_POST['language'], Languages::$all)) {
+            $language = $_POST['language'];
+            $words = array_values(array_filter(
+                $_POST,
+                fn($key) => preg_match('/^word-[0-9]+$/', $key),
+                ARRAY_FILTER_USE_KEY
+            ));
 
-        header('Content-Type: text/plain');
-        // header('Content-Type: text/tab-separated-values');
+            if (!empty($words)) {
+                header('Content-Type: text/plain');
+                // header('Content-Type: text/tab-separated-values');
 
-        $this->render->setTemplate('Tsv')
-                     ->process([
-                        'cards' => $this->getCards($words, $language),
-                        'languageIsValid' => $this->languageIsValid,
-                    ]);
+                $this->render->setTemplate('Tsv')->process([
+                    'cards' => $this->getCards($words, $language),
+                ]);
+
+                return;
+            }
+        }
+
+        throw new HTTPException(400);
     }
 }
