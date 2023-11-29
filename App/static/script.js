0eee5af7e3d886411152b6e46ca329b751fd627a
Cyril Augier <cicxjo@posteo.net>
2023-11-29 15:44:22 +0100

    Refacto JS + style Standard

--- App/static/script.js
+++ App/static/script.js
@@ -1,37 +1,36 @@
-const website = location.protocol.concat('//').concat(location.host); // or location.origin
-const url = website.concat('/template/word');
+const website = window.location.protocol.concat('//').concat(window.location.host) // or location.origin
+const url = website.concat('/template/word')
 
-const buttonAddInputElt = document.getElementById('btn-add-input');
-const inputsWrapperElt = document.getElementById('inputs-wrapper');
-let inputId = 0;
+const buttonAddInputElt = document.getElementById('btn-add-input')
+const inputsWrapperElt = document.getElementById('inputs-wrapper')
+let inputId = 0
 
 const fetchInsertNextInputElement = async () => {
-  inputId++;
+  inputId++
 
   const data = new URLSearchParams()
-  data.append('id', inputId);
+  data.append('id', inputId)
 
   const payload = {
     method: 'POST',
-    body: data,
-  };
+    body: data
+  }
 
-  const response = await fetch(url, payload);
-  const text = await response.text();
-  const parser = new DOMParser();
-  const tmpDocument = parser.parseFromString(text, 'text/html');
-  const inputWrapperElt = tmpDocument.getElementById('input-wrapper-'.concat(inputId));
-  const buttonDeleteInputElt = tmpDocument.getElementById('btn-delete-input-'.concat(inputId));
+  const response = await fetch(url, payload)
+  const text = await response.text()
+  const tmpDocument = new window.DOMParser().parseFromString(text, 'text/html')
+  const inputWrapperElt = tmpDocument.getElementById('input-wrapper-'.concat(inputId))
+  const buttonDeleteInputElt = tmpDocument.getElementById('btn-delete-input-'.concat(inputId))
 
   buttonDeleteInputElt.addEventListener('click', event => {
-    inputWrapperElt.remove(inputWrapperElt);
-    event.preventDefault();
-  });
+    inputWrapperElt.remove(inputWrapperElt)
+    event.preventDefault()
+  })
 
-  inputsWrapperElt.append(inputWrapperElt);
-};
+  inputsWrapperElt.append(inputWrapperElt)
+}
 
 buttonAddInputElt.addEventListener('click', event => {
-  fetchInsertNextInputElement();
-  event.preventDefault();
-});
+  fetchInsertNextInputElement()
+  event.preventDefault()
+})
